ea8b5a85314f1abadabbeaa33b45d82cb3f7cef0
tapin13 <tapin13@gmail.com>
2017-10-20 02:12:19 +0300

    fix pg

--- index.js
+++ index.js
@@ -30,16 +30,21 @@ app.get('/times', function(request, response) {
 });
 
 app.get('/db', function (request, response) {
-    Pool.connect(process.env.DATABASE_URL, function(err, client, done) {
-       client.query('Select * from test_table', function(err, result) {
-           done();
-           if(err) {
-               console.error(err);
-               response.send("Error: " + err);
-           } else {
-               response.render('pages/db', { results: result.rows });
-           }
-       });
+    Pool.connect(process.env.DATABASE_URL, function (err, client, done) {
+        if (err) {
+            console.error(err);
+            response.send("Error: " + err);
+            return false;
+        }
+        client.query('Select * from test_table', function (err, result) {
+            done();
+            if (err) {
+                console.error(err);
+                response.send("Error: " + err);
+            } else {
+                response.render('pages/db', {results: result.rows});
+            }
+        });
     });
 });
 
